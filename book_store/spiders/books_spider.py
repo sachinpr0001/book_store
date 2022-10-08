9b185b6e135d88bc2c7740e1dc48a00fb82ed33a
sachinpr0001 <sachinpr0001@gmail.com>
2022-10-08 23:55:27 +0530

    added venv

--- book_store/spiders/books_spider.py
+++ book_store/spiders/books_spider.py
@@ -1,13 +1,23 @@
+"""Scraper for books catalogue
+
+Yields:
+    csv: all_bookss.csv
+"""
 import scrapy
-import pandas as pd
 
 class BooksSpider(scrapy.Spider):
+    """book scraper class
+
+    Args:
+        scrapy
+
+    Yields:
+        response
+    """
     name = "books_spider"
 
     def start_requests(self):
-        urls = [
-            'https://books.toscrape.com/catalogue/page-1.html'
-        ]
+        urls = ["https://books.toscrape.com/catalogue/page-1.html"]
 
         # Generator Function
         for url in urls:
@@ -15,19 +25,18 @@ class BooksSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        for q in response.css('article.product_pod'):
-            image_url = q.css('img.thumbnail::attr(src)').get()
-            book_title = q.css('::attr(title)').get()
-            product_price = q.css('p.price_color::text').get() 
-        
+        for value in response.css("article.product_pod"):
+            image_url = value.css("img.thumbnail::attr(src)").get()
+            book_title = value.css("::attr(title)").get()
+            product_price = value.css("p.price_color::text").get()
 
             yield {
-            'image_url':image_url,
-            'book_title':book_title,
-            'product_price':product_price,
+                "image_url": image_url,
+                "book_title": book_title,
+                "product_price": product_price,
             }
 
-        next_page = response.css('li.next a::attr(href)').get()
+        next_page = response.css("li.next a::attr(href)").get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
